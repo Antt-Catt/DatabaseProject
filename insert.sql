35f5cfe40bc4fc06957c6224b497ec3b8508647f
Julia Dunoyer <jdunoyer@enseirb-matmeca.fr>
2023-11-20 17:10:34 +0100

    modif insert

--- insert.sql
+++ insert.sql
@@ -27,30 +27,19 @@ INSERT INTO etudiant (nom_etudiant, prenom_etudiant) VALUES
 
 --voiture
 INSERT INTO voiture (type, couleur, nombre_de_places, etat, conducteur) VALUES 
-('CHARETTE', 'VERT', 5, 'NEUF', 1);
-('4X4', 'ORANGE', 4, 'NEUF', 2);
-('ELECTRIQUE', 'ORANGE', 4, 'NEUF', 1);
-('FAMILIALE', 'ROUGE', 10, 'NEUF', 3);
-('4X4', 'NOIR', 5, 'NEUF', 4);
+('CHARETTE', 'VERT', 5, 'NEUF', 1),
+('4X4', 'ORANGE', 4, 'NEUF', 2),
+('ELECTRIQUE', 'ORANGE', 4, 'NEUF', 1),
+('FAMILIALE', 'ROUGE', 10, 'NEUF', 3),
+('4X4', 'NOIR', 5, 'NEUF', 4),
 ('ELECTRIQUE', 'BLANC', 3, 'VIEUX', 5);
 
 
 
 --trajet
 INSERT INTO trajet (instant_depart, frais_par_passager, conducteur, id_voiture) VALUES 
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
+(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1),
 (TO_TIMESTAMP('11-JAN-1975-10-10-00', 'DD-MON-YYYY-HH-MI-SS'),5,4,2);
-
-
-(TO_TIMESTAMP('26-JUN-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
-(TO_TIMESTAMP('13-OCT-1921-12-30-00', 'DD-MON-YYYY-HH-MI-SS'),30,1,1);
 
 
 --ville
@@ -67,9 +56,9 @@ INSERT INTO ville (nom_ville) VALUES
 
 --etapes
 INSERT INTO etape (duree, distance, id_trajet, ville_depart, ville_arrivee) VALUES 
-(30, 35, 1, 1, 2);
-(400, 100, 2, 1, 3);
-(50, 35, 2, 1, 7);
+(30, 35, 1, 1, 2),
+(400, 100, 2, 1, 3),
+(50, 35, 2, 1, 7),
 (350, 65, 2, 7, 3);
 
 
@@ -80,9 +69,9 @@ INSERT INTO avis VALUES
 
 --inscription
 INSERT INTO inscription VALUES 
-(2, 1);
-(5, 2);
-(6, 2);
+(2, 1),
+(5, 2),
+(6, 2),
 (7, 2);
 
 
