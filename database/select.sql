0d20b0e473c022c75bf5526da016f9f84a293937
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-12-04 17:41:39 +0100

    added first request

--- database/select.sql
+++ database/select.sql
@@ -1,4 +1,22 @@
--- Informations sur les conducteurs, passagers (pour un etudiant, on obtient les trajets auxquels il a participé, ceux qu'il a proposé) 
+-- Informations sur les conducteurs, passagers (pour un etudiant, on obtient les trajets auxquels il a participé, ceux qu'il a proposé)
+
+SELECT 'Conducteur' AS role, DATE(t.instant_depart) as date_depart, vd.nom_ville AS ville_depart, va.nom_ville AS ville_arrivee, t.frais_par_passager
+FROM  trajet t
+JOIN etape e ON t.id_trajet = e.id_trajet
+JOIN ville vd ON e.ville_depart = vd.id_ville
+JOIN ville va ON e.ville_arrivee = va.id_ville
+WHERE t.conducteur = 1
+
+UNION
+
+SELECT 'Passager' AS role, DATE(t.instant_depart) as date_depart, vd.nom_ville AS ville_depart, va.nom_ville AS ville_arrivee, t.frais_par_passager
+FROM trajet t
+JOIN inscription i ON t.id_trajet = i.id_trajet
+JOIN etape e ON t.id_trajet = e.id_trajet
+JOIN ville vd ON e.ville_depart = vd.id_ville
+JOIN ville va ON e.ville_arrivee = va.id_ville
+WHERE i.id_etudiant = 1
+;
 
 -- Liste des véhicules disponibles pour un jour donné pour une ville donnée
 SELECT vo.*
@@ -8,7 +26,6 @@ INNER JOIN etape e ON t.id_trajet = e.id_trajet
 INNER JOIN ville vi ON e.ville_depart = vi.id_ville
 WHERE vi.nom_ville = 'BORDEAUX'
 AND DATE_TRUNC('day', t.instant_depart)::DATE = '1921-10-13'
-
 ;
 
 -- Les trajets proposés dans un intervalle de jours donné la liste des villes renseignées entre le campus et une ville donnée
@@ -16,10 +33,10 @@ AND DATE_TRUNC('day', t.instant_depart)::DATE = '1921-10-13'
 -- Les trajets pouvant desservir une ville donnée dans un intervalle de temps
 SELECT t.id_trajet, t.instant_depart, v_dep.nom_ville AS ville_depart, v_arr.nom_ville AS ville_arrivee
 FROM trajet t
-JOIN etape et_dep ON t.id_trajet = et_dep.id_trajet
-JOIN etape et_arr ON t.id_trajet = et_arr.id_trajet
-JOIN ville v_dep ON et_dep.ville_depart = v_dep.id_ville
-JOIN ville v_arr ON et_arr.ville_arrivee = v_arr.id_ville
+INNER JOIN etape et_dep ON t.id_trajet = et_dep.id_trajet
+INNER JOIN etape et_arr ON t.id_trajet = et_arr.id_trajet
+INNER JOIN ville v_dep ON et_dep.ville_depart = v_dep.id_ville
+INNER JOIN ville v_arr ON et_arr.ville_arrivee = v_arr.id_ville
 WHERE (v_dep.nom_ville = 'BORDEAUX')
 AND DATE_TRUNC('day', t.instant_depart)::DATE >= '1921-10-10'
 AND DATE_TRUNC('day', t.instant_depart)::DATE <= '1921-10-20'
