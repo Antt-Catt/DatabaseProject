e3c27e279948efbce80903429cae380b01f4b385
HAMOUCHE Luxel <luxel.hamouche@bordeaux-inp.fr>
2023-12-04 21:12:18 +0100

    Updated nav bar

--- website/_nav.php
+++ website/_nav.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="index.php">Accueil</a></li>
     <li><a href="students.php">Etudiants</a></li>
-    <li><a href="#">Voitures</a></li>
+    <li><a href="cars.php">Voitures</a></li>
     <li><a href="#">Trajets</a></li>
 </ul>
