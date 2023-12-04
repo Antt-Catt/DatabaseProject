bf011bc632d13e65418681f278bd06ee9cc4ddab
HAMOUCHE Luxel <luxel.hamouche@bordeaux-inp.fr>
2023-12-04 23:11:18 +0100

    Updated get_cars

--- website/get_cars.php
+++ website/get_cars.php
@@ -8,30 +8,30 @@ try {
         $filterCity = isset($_GET['filterCity']) ? $_GET['filterCity'] : '';
         $filterDate = isset($_GET['filterDate']) ? $_GET['filterDate'] : '';
 
-        $query = "SELECT vo.*
+        $query = "SELECT vo.*, et.*, t.*, vi_arrivee.nom_ville AS nom_ville_arrivee, vi_depart.nom_ville AS nom_ville_depart
                   FROM voiture vo
                   INNER JOIN trajet t ON vo.id_voiture = t.id_voiture
                   INNER JOIN etape e ON t.id_trajet = e.id_trajet
-                  INNER JOIN ville vi ON e.ville_depart = vi.id_ville
+                  INNER JOIN ville vi_depart ON e.ville_depart = vi_depart.id_ville
+                  INNER JOIN ville vi_arrivee ON e.ville_arrivee = vi_arrivee.id_ville
+                  INNER JOIN etudiant et ON vo.conducteur = et.id_etudiant
                   WHERE 1 = 1";
 
-
         if (!empty($filterCity)) {
-            $query .= " AND LOWER(vi.nom_ville) LIKE :city";
+            $query .= " AND LOWER(vi_depart.nom_ville) LIKE :city";
         }
-        
+
         if (!empty($filterDate)) {
             $formattedDate = date('Y-m-d', strtotime($filterDate));
             $query .= " AND DATE_TRUNC('day', t.instant_depart)::DATE = :date";
         }
-
 
         $stmt = $conn->prepare($query);
 
         if (!empty($filterCity)) {
             $stmt->bindValue(':city', '%' . strtolower($filterCity) . '%');
         }
-        
+
         if (!empty($filterDate)) {
             $stmt->bindValue(':date', '%' . $formattedDate . '%');
         }
@@ -39,9 +39,19 @@ try {
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
-            echo "<table>";
+            $titles = ["Type", "Couleur", "Nom du conducteur", "Prénom du conducteur", "Instant du départ", "Frais par passager", "Ville de départ", "Ville d'arrivée"];
+            echo "<tr>";
+            foreach ($titles as $t) {
+                echo "<th>" . $t . "</th>";
+            }
+            echo "</tr>";
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo "<tr><td>" . $row['id_voiture'] . "</td><td>" . $row['type'] . "</td><td>" . $row['couleur'] . "</td></tr>";
+                $elements = ["type", "couleur", "nom_etudiant", "prenom_etudiant", "instant_depart", "frais_par_passager", "nom_ville_depart", "nom_ville_arrivee"];
+                echo "<tr>";
+                foreach ($elements as $e) {
+                    echo "<td>" . $row[$e] . "</td>";
+                }
+                echo "</tr>";
             }
             echo "</table>";
         } else {
