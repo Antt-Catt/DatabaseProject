b3489351c3bdd12f8c3df8a8af51934ec7f23685
Julia Dunoyer <jdunoyer@enseirb-matmeca.fr>
2023-12-05 16:51:11 +0100

    trajets

---
+++ website/get_trajets.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'db_connection.php';
+
+try {
+    $conn = connectDB();
+
+    if ($conn) {
+        $filterCity = isset($_GET['filterCity']) ? $_GET['filterCity'] : '';
+        $filterDate = isset($_GET['filterDate']) ? $_GET['filterDate'] : '';
+
+        $query = "SELECT vo.*, et.*, t.*, vi_arrivee.nom_ville AS nom_ville_arrivee, vi_depart.nom_ville AS nom_ville_depart
+                  FROM voiture vo
+                  INNER JOIN trajet t ON vo.id_voiture = t.id_voiture
+                  INNER JOIN etape e ON t.id_trajet = e.id_trajet
+                  INNER JOIN ville vi_depart ON e.ville_depart = vi_depart.id_ville
+                  INNER JOIN ville vi_arrivee ON e.ville_arrivee = vi_arrivee.id_ville
+                  INNER JOIN etudiant et ON vo.conducteur = et.id_etudiant
+                  WHERE 1 = 1";
+                 
+
+         if (!empty($filterCity)) {
+            $query .= " AND LOWER(vi_depart.nom_ville) LIKE :city";
+         }
+
+         if (!empty($filterDate)) {
+             $formattedDate = date('Y-m-d', strtotime($filterDate));
+             $query .= " AND DATE_TRUNC('day', t.instant_depart)::DATE = :date";
+        }
+
+        $stmt = $conn->prepare($query);
+
+         if (!empty($filterCity)) {
+             $stmt->bindValue(':city', '%' . strtolower($filterCity) . '%');
+         }
+
+         if (!empty($filterDate)) {
+             $stmt->bindValue(':date', '%' . $formattedDate . '%');
+         }
+
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $titles = ["Type", "Couleur", "Nom du conducteur", "Prénom du conducteur", "Instant du départ", "Frais par passager", "Ville de départ", "Ville d'arrivée"];
+
+            echo "<tr>";
+            foreach ($titles as $t) {
+                echo "<th>" . $t . "</th>";
+            }
+            echo "</tr>";
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $elements = ["type", "couleur", "nom_etudiant", "prenom_etudiant", "instant_depart", "frais_par_passager", "nom_ville_depart", "nom_ville_arrivee"];
+                // $elements = ["type", "couleur", "nom_etudiant", "prenom_etudiant"];
+
+                echo "<tr>";
+                foreach ($elements as $e) {
+                    echo "<td>" . $row[$e] . "</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "Aucun résultat trouvé.";
+        }
+    }
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
---
+++ website/trajets.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Covoiturage</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <?php
+        include $_SERVER['DOCUMENT_ROOT'] . '/_nav.php'; ?>
+    </header>
+    <main>
+        <div>
+            <label for="filterCity">Filtrer par ville :</label>
+            <input type="text" id="filterCity" name="filterCity">
+            <br>
+            <label for="filterDate">Filtrer par date :</label>
+            <input type="date" id="filterDate" name="filterDate">
+        </div>
+        <!-- <form method="POST" action="insert_trajets.php" id="addCarForm">
+            <label for="type">Type :</label>
+            <input type="text" id="type" name="type" required>
+
+            <label for="couleur">Couleur :</label>
+            <input type="text" id="couleur" name="couleur" required>
+
+            <label for="ndp">Nombre de places :</label>
+            <input type="text" id="ndp" name="ndp" required>
+
+            <label for="etat">Etat :</label>
+            <input type="text" id="etat" name="etat" required>
+
+            <label for="conducteur">Id du conducteur :</label>
+            <input type="text" id="conducteur" name="conducteur" required>
+
+            <input type="submit" value="Ajouter vehicule">
+        </form> -->
+
+        <div id="trajetsList"></div>
+
+        <script>
+            $(document).ready(function() {
+                function getCars(filterCity = '', filterDate = '') {
+                    $.ajax({
+                        url: 'get_trajets.php',
+                        method: 'GET',
+                        data: {
+                            filterCity: filterCity,
+                            filterDate: filterDate
+                        },
+                        success: function(response) {
+                            $('#trajetsList').html(response);
+                        }
+                    });
+                }
+
+                getCars();
+
+                $('#filterCity').on('input', function() {
+                    getCars($(this).val().toLowerCase(), $('#filterDate').val());
+                });
+
+                $('#filterDate').on('change', function() {
+                    getCars($('#filterCity').val().toLowerCase(), $(this).val());
+                });
+            });
+        </script>
+    </main>
+</body>
+
+</html>
