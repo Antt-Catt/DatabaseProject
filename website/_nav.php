54cf67a25145885368c8c554990972b389d091cf
HAMOUCHE Luxel <luxel.hamouche@bordeaux-inp.fr>
2023-12-06 21:36:50 +0100

    typo on index

--- website/_nav.php
+++ website/_nav.php
@@ -3,7 +3,7 @@
     <li><a href="students.php">Etudiants</a></li>
     <li><a href="cars.php">Voitures</a></li>
     <li><a href="trajets.php">Trajets</a></li>
-    <li><a href="registrations.php">inscription</a></li>
+    <li><a href="registrations.php">Inscriptions</a></li>
     <li><a href="avis.php">Avis</a></li>
     <li><a href="stats.php">Statistiques</a></li>
 </ul>
