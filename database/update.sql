9d8f8e39348389a17ff482a9b0fd233d047767c4
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-12-06 22:44:23 +0100

    added del requests for all entities

--- database/update.sql
+++ database/update.sql
@@ -1,30 +1,50 @@
---Commandes de mise à jour des tables
+-- ============================================================
+--   Mise à jour des tables                                      
+-- ============================================================
 
---AVIS
---ajoute un nouvel avis dans la table avis correspondante - les champs sont remplis à partir d'un formulaire rempli par l'utilisateur
-INSERT INTO avis (id_trajet, auteur, destinataire, note, commentaire) VALUES (:id_trajet, :auteur, :destinataire, :note, :commentaire);
 
---ETAPES
---ajoute une nouvelle étape dans la table etape correspondante - les champs sont remplis à partir d'un formulaire rempli par l'utilisateur
+-- Les valeurs à renseigner par l'utilisateur sont marquées par un caractère ':'
+
+
+-- ============================================================
+--   Table : AVIS
+-- ============================================================
+INSERT INTO avis (id_trajet, auteur, destinataire, note, commentaire) VALUES (:id_trajet, :auteur, :destinataire, :note, :commentaire); -- ajout
+DELETE FROM avis WHERE auteur = :auteur AND destinataire = :destinataire AND id_trajet = :id_trajet; -- suppression
+
+-- ============================================================
+--   Table : ETAPE
+-- ============================================================
 INSERT INTO etape (duree, distance, id_trajet, ville_depart, ville_arrivee) VALUES (:duree, :distance, :id_trajet, :ville_depart, :ville_arrivee);
-
---STUDENTS
---ajoute un nouvel étudiant dans la table etudiant correspondante - les champs sont remplis à partir d'un formulaire rempli par l'utilisateur
-INSERT INTO etudiant (nom_etudiant, prenom_etudiant) VALUES (:nom, :prenom);
---supprime un etudiant de la table etudiant à partir de son identifiant (renseigné par l'utilisateur dans un formulaire)
 DELETE FROM etudiant WHERE id_etudiant = :id_etudiant;
 
---TRAJETS
---ajoute un nouveau trajet dans la table trajet correspondante - les champs sont remplis à partir d'un formulaire rempli par l'utilisateur
+-- ============================================================
+--   Table : ETUDIANT
+-- ============================================================
+INSERT INTO etudiant (nom_etudiant, prenom_etudiant) VALUES (:nom, :prenom);
+DELETE FROM etudiant WHERE id_etudiant = :id_etudiant;
+
+-- ============================================================
+--   Table : TRAJET
+-- ============================================================
 INSERT INTO trajet (instant_depart, frais_par_passager, conducteur, id_voiture) VALUES (:instant_depart, :frais_par_passager, :conducteur, :id_voiture);
+DELETE FROM trajet WHERE id_trajet = :id_trajet;
 
---VILLES
---ajoute à la table ville une nouvelle ville - le champ de la table est rempli à partir d'un formulaire rempli par l'utilisateur
+-- ============================================================
+--   Table : VILLE
+-- ============================================================
 INSERT INTO ville (nom_ville) VALUES (:nom_ville);
+DELETE FROM etudiant WHERE id_etudiant = :id_etudiant;
 
---VOITURES
---ajoute une nouvelle voiture dans la table voiture correspondante - les champs sont remplis à partir d'un formulaire rempli par l'utilisateur
+-- ============================================================
+--   Table : VOITURE
+-- ============================================================
 INSERT INTO voiture (type, couleur, nombre_de_places, etat, conducteur) VALUES (:type, :couleur, :ndp, :etat, :conducteur);
---supprime une voiture de la table voiture à partir de son identifiant (renseigné par l'utilisateur dans un formulaire)
 DELETE FROM voiture WHERE id_voiture = :id_voiture;
 
+-- ============================================================
+--   Table : INSCRIPTION
+-- ============================================================
+INSERT INTO inscription (id_etudiant, id_trajet, id_ville, acceptation) VALUES (:id_etudiant, :id_trajet, :id_ville, false);
+UPDATE inscription SET acceptation = TRUE WHERE id_etudiant = :id_etudiant AND id_trajet = :id_trajet AND id_ville = :id_ville;
+DELETE FROM inscription WHERE id_etudiant = :id_etudiant AND id_trajet = :id_trajet AND id_ville = :id_ville;
